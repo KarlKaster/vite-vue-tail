918505c2b2d1979bd22f86dad0dde2ec5d8071bb
Kaster <karl.kaster@adesso.de>
2022-05-14 09:09:21 +0200

    added scripts for linting

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -17,33 +17,42 @@ console.log(store.state.count);
 </script>
 
 <template>
-<div>
-
+  <div>
     <h1>{{ msg }}</h1>
     <p>
-        Recommended IDE setup:
-        <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
-        +
-        <a href="https://github.com/johnsoncodehk/volar" target="_blank"
-            >Volar</a
-        >
+      Recommended IDE setup:
+      <a
+        href="https://code.visualstudio.com/"
+        target="_blank"
+      >VS Code</a>
+      +
+      <a
+        href="https://github.com/johnsoncodehk/volar"
+        target="_blank"
+      >Volar</a>
     </p>
 
     <p>See <code>README.md</code> for more information.</p>
 
     <p>
-        <a href="https://vitejs.dev/guide/features.html" target="_blank">
-            Vite Docs
-        </a>
-        |
-        <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
+      <a
+        href="https://vitejs.dev/guide/features.html"
+        target="_blank"
+      >
+        Vite Docs
+      </a>
+      |
+      <a
+        href="https://v3.vuejs.org/"
+        target="_blank"
+      >Vue 3 Docs</a>
     </p>
 
     <p>
-        Edit
-        <code>components/HelloWorld.vue</code> to test hot module replacement.
+      Edit
+      <code>components/HelloWorld.vue</code> to test hot module replacement.
     </p>
-    </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
